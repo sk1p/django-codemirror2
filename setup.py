7e4d67468fe8c6d43cb42cee3ba70e815afcd4fa
Alexander Clausen <alex@gc-web.de>
2011-07-08 20:53:53 +0200

    ship the examples project

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ def read(fname):
 README = read("README.rst")
 
 setup(name="django-codemirror2",
-    version="0.0.1",
+    version="0.0.2-dev",
     author_email="alex@gc-web.de",
     author="Alexander Clausen",
     description="Django widgets for replacing textareas with CodeMirror2, an in-browser code editor",
+    url="https://github.com/sk1p/django-codemirror2",
     long_description=README,
     packages=["codemirror2"],
     package_data = {'codemirror2': [os.path.join("templates", "codemirror2", "*")]},
-    include_package_data=True,
 )
