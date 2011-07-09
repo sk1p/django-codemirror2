d14ae7532ff2ae04445fc040a2c65530e51d96c7
Alexander Clausen <alex@gc-web.de>
2011-07-09 02:26:33 +0200

    I'm a packaging noob.

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from distutils.core import setup
-from distutils.command.install import INSTALL_SCHEMES
+from setuptools import setup, find_packages
 import os
 
 BASE_DIR = os.path.dirname(__file__)
@@ -7,28 +6,31 @@ BASE_DIR = os.path.dirname(__file__)
 def read(fname):
     return open(os.path.join(BASE_DIR, fname)).read()
 
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
-
-data_files = []
-for dirpath, dirnames, filenames in os.walk(os.path.join(BASE_DIR, "codemirror2", "static")):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
-
 README = read("README.rst")
 
-setup(name="django-codemirror2",
-    version="0.0.3",
+setup(
+    name="django-codemirror2",
+    version="0.0.4",
     author_email="alex@gc-web.de",
     author="Alexander Clausen",
-    description="Django widgets for replacing textareas with CodeMirror2, an in-browser code editor",
     url="https://github.com/sk1p/django-codemirror2",
+    description="Django widgets for replacing textareas with CodeMirror2, an in-browser code editor",
     long_description=README,
-    packages=["codemirror2"],
-    package_data = {'codemirror2': [os.path.join("templates", "codemirror2", "*")]},
-    data_files=data_files,
+    setup_requires = [
+        "setuptools_git>=0.3.4",
+    ],
+
+    packages=find_packages(exclude=("examples",)),
     include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: JavaScript',
+        'Topic :: Text Editors',
+    ],
 )
