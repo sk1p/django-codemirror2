7715e96add15ae1be9dbb629e93556e18f7a48cc
Alexander Clausen <alex@gc-web.de>
2024-02-23 00:30:54 +0100

    Make the example kind-of run again
    Very minimal, needs more updates.

--- examples/testapp/models.py
+++ examples/testapp/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 
 
-@python_2_unicode_compatible
 class TestCss(models.Model):
     title = models.CharField(max_length=255)
     css = models.TextField()
@@ -11,7 +9,6 @@ class TestCss(models.Model):
         return self.title
 
 
-@python_2_unicode_compatible
 class TestHTML(models.Model):
     title = models.CharField(max_length=255)
     html = models.TextField()
